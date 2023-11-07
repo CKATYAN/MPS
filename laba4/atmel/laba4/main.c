81b418f8c8e22da2fcd40da46c7522a7f7847fd5
CKATYAN <ct.rainfox2017@gmail.com>
2023-11-07 16:33:02 +0700

    Update main.c

--- laba4/atmel/laba4/main.c
+++ laba4/atmel/laba4/main.c
@@ -7,6 +7,7 @@
 
 #include "FreeRTOS.h"
 #include "task.h"
+#include <util/delay.h>
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <stdbool.h>
@@ -31,38 +32,13 @@ int flagMoovingSpeed = 0;
 int counter = 0;
 int tick = 0;
 
-
-
-
-
-void init_timer(){
-    TCNT1 = 0;
-    TCCR1B |= (1<<CS10) | (0<<CS11) | (1<<CS12); // установка делителя частоты в 1024
-    TIMSK |= (1 << OCIE1B);
-    OCR1BH = 0b00111101;
-    OCR1BL = 0b00001001;
-    sei();
-}
-
-ISR(TIMER1_COMPB_vect) {
-   	computeTimeArray();
-   	TCNT1 = 0;
-}
-
-
-
-
-
-
-
-
-
-
 void vDisplayClockTask (void *pvParameters);
+void vButtonHandlerTask (void *pvParameters);
 
 int main(void)
 {
 	xTaskCreate(vDisplayClockTask, (char*) "DisplayClockTask", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY + 1, NULL);
+	xTaskCreate(vButtonHandlerTask, (char*) "ButtonHandlerTask", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY + 1, NULL);
 	vTaskStartScheduler();
     /* Replace with your application code */
     while (1)
@@ -72,6 +48,37 @@ int main(void)
 
 void vApplicationIdleHook() {} // empty func to rid of idle-thread
 
+void initStopButton()
+{
+	DDRD = 0;
+	MCUCR = (1<<ISC01) | (1<<ISC00);
+	GICR |= (1<<INT0);
+	GIFR = (1<<INTF0);
+}
+
+void vButtonHandlerTask(void *pvParameters)
+{
+	initStopButton();
+	while(1)
+	{
+		vTaskDelay(1);
+	}
+}
+
+void init_timer(){
+	TCNT1 = 0;
+	TCCR1B |= (1<<CS10) | (1<<CS11) | (0<<CS12);
+	TIMSK |= (1 << OCIE1B);
+	OCR1BH = 0b00111101;
+	OCR1BL = 0b00001001;
+	sei();
+}
+
+ISR(TIMER1_COMPB_vect) {
+	computeTimeArray();
+	TCNT1 = 0;
+}
+
 void vDisplayClockTask(void *pvParameters)
 {	
     LCDInit(); 
@@ -80,12 +87,185 @@ void vDisplayClockTask(void *pvParameters)
 	init_timer();
     while (1) 
     {
-        computePosition(); 
-        displayTime(positionOnDisplay); 
         displayData(positionOnDisplay);
-		//counter = xTaskGetTickCount();
+        displayTime(positionOnDisplay);
+        if (!stop_flag)
+        {
+	        computePosition();
+        }
+        else
+        {
+	        if (PIND == CHANGING_UP)
+	        {
+		        cli();
+		        GICR &= (0<<INT0);
+		        changingMode();
+	        }
+        }
     }
 	vTaskDelete(NULL);
+}
+
+void togglePosition()
+{
+	if (!isFirstLine && shiftCursorOnDisplay > 2 && shiftCursorOnDisplay < 6)
+	{
+		toggleMonth();
+		return;
+	}
+	LCDKursorPosihin(!isFirstLine, shiftCursorOnDisplay + positionOnDisplay);
+	LCDWriteByte(LCD_DR, ' ');
+	_delay_ms(150);
+	if (isFirstLine)
+	displayTime(positionOnDisplay);
+	else
+	displayData(positionOnDisplay);
+	_delay_ms(150);
+}
+
+void toggleMonth()
+{
+	LCDKursorPosihin(1, positionOnDisplay+3);
+	for (int i = 0; i < 3; i++)
+	LCDWriteByte(LCD_DR, ' ');
+	_delay_ms(200);
+	displayData(positionOnDisplay);
+	_delay_ms(200);
+}
+
+void changingMode()
+{
+	isFirstLine = true;
+	shiftCursorOnDisplay = 3;
+	isMovingToTheRIght = true;
+	while(1)
+	{
+		togglePosition();
+		switch(PIND)
+		{
+			case CHANGING:
+			sei();
+			initStopButton();
+			return 0;
+			break;
+			case LEFT:
+			isMovingToTheRIght = false;
+			shiftCursorOnDisplay--;
+			checkCursorPosition();
+			_delay_ms(300);
+			break;
+			case RIGHT:
+			isMovingToTheRIght = true;
+			shiftCursorOnDisplay++;
+			checkCursorPosition();
+			_delay_ms(300);
+			break;
+			case CHANGING_UP:
+			if (isFirstLine)
+			changePositionTime(shiftCursorOnDisplay - 3, 1);
+			else
+			changePositionData(shiftCursorOnDisplay, 1);
+			_delay_ms(300);
+			break;
+			case CHANGING_DOWN:
+			if (isFirstLine)
+			changePositionTime(shiftCursorOnDisplay - 3, -1);
+			else
+			changePositionData(shiftCursorOnDisplay, -1);
+			_delay_ms(300);
+			break;
+			default:break;
+		}
+		
+		
+	}
+}
+
+void checkCursorPosition()
+{
+	if (isFirstLine)
+	{
+		switch(shiftCursorOnDisplay-3)
+		{
+			case -1:
+			changeLine();
+			break;
+			case 2:
+			shiftFromColon();
+			break;
+			case 5:
+			shiftFromColon();
+			break;
+			case 8:
+			changeLine();
+			break;
+			default:
+			return;
+		}
+	}
+	else
+	switch(shiftCursorOnDisplay)
+	{
+		case -1:
+		changeLine();
+		break;
+		case 2:
+		shiftFromColon();
+		break;
+		case 4:
+		shiftFromMonth();
+		break;
+		case 5:
+		shiftFromMonth();
+		break;
+		case 6:
+		shiftFromColon();
+		break;
+		case 11:
+		changeLine();
+		break;
+		default:
+		return;
+	}
+}
+
+void shiftFromMonth()
+{
+	if (isMovingToTheRIght)
+	shiftCursorOnDisplay += 3;
+	else
+	shiftCursorOnDisplay -= 3;
+}
+
+void shiftFromColon()
+{
+	if (isMovingToTheRIght)
+	shiftCursorOnDisplay++;
+	else
+	shiftCursorOnDisplay--;
+}
+
+void changeLine()
+{
+	isFirstLine = !isFirstLine;
+	if (isFirstLine)
+	{
+		if (isMovingToTheRIght)
+		{
+			shiftCursorOnDisplay = 3;
+			return;
+		}
+		shiftCursorOnDisplay = 10;
+	}
+	else
+	{
+		if (isMovingToTheRIght)
+		{
+			shiftCursorOnDisplay = 0;
+			return;
+		}
+		shiftCursorOnDisplay = 10;
+	}
 }
 
 void computePosition()
@@ -119,4 +299,10 @@ void computePosition()
 	isGoingLeft = !isGoingLeft;
 
 	isGoingLeft ? positionOnDisplay-- : positionOnDisplay++;
+}
+
+ISR(INT0_vect)
+{
+	stop_flag = !stop_flag;
+	_delay_ms(60);
 }
